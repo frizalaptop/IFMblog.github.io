e6c5227d9f1692519429a3e739cad00a1da7a530
muhammadfriza99 <frizamuhammadcrb1234@gmail.com>
2023-01-27 20:55:27 +0700

    Menyelesaikan blog b.ing

--- script/script.js
+++ script/script.js
@@ -2,12 +2,15 @@ window.onscroll = function () {
   myFunction();
 };
 const navbar = document.querySelector('.navbar');
+const allMenu = document.querySelector('.allMenu');
 const sticky = navbar.offsetTop;
 function myFunction() {
   if (window.pageYOffset >= sticky) {
     navbar.classList.add('sticky');
+    allMenu.classList.add('sticky1');
   } else {
     navbar.classList.remove('sticky');
+    allMenu.classList.remove('sticky1');
   }
 }
 
